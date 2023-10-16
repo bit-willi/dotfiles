358bd848ec871fe30c2bf8c66a41e20238ce0f1d
Willian de Souza <willian.souza.ti@gmail.com>
2023-10-16 17:59:27 -0300

    vim: add mappings for telescope

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -1,14 +1,23 @@
+local actions = require("telescope.actions")
+
 require("telescope").setup{
     defaults = {
-        file_ignore_patterns = {".git", "node_modules", "vendor"}
+        file_ignore_patterns = {".git", "node_modules", "vendor"},
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+        },
     }
 }
 
 require("telescope").load_extension("harpoon")
 
-vim.keymap.set("n", "<leader>sf",  require("telescope.builtin").find_files)
-vim.keymap.set("n", "<leader>sh",  require("telescope.builtin").help_tags)
-vim.keymap.set("n", "<leader>sw",  require("telescope.builtin").grep_string)
-vim.keymap.set("n", "<leader>sg",  require("telescope.builtin").live_grep)
-vim.keymap.set("n", "<leader>sb",  require("telescope.builtin").buffers)
-vim.keymap.set("n", "<leader>/",  require("telescope.builtin").current_buffer_fuzzy_find)
+vim.keymap.set("n", "<leader>sf",  require("telescope.builtin").find_files, { desc = "Search files in cwd" })
+vim.keymap.set("n", "<leader>sh",  require("telescope.builtin").help_tags, { desc = "Search help tags in cwd" })
+vim.keymap.set("n", "<leader>sw",  require("telescope.builtin").grep_string, { desc = "Search string in cwd" })
+vim.keymap.set("n", "<leader>sg",  require("telescope.builtin").live_grep, { desc = "Grep files in cwd" })
+vim.keymap.set("n", "<leader>sb",  require("telescope.builtin").buffers, { desc = "Search buffers in cwd" })
+vim.keymap.set("n", "<leader>/",  require("telescope.builtin").current_buffer_fuzzy_find, { desc = "Search strings in current buffer" })

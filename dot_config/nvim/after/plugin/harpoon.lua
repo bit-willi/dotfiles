85be9125f5a85b9ea98399e2e4281dcfc0ca3b13
Willian de Souza <willian.souza.ti@gmail.com>
2023-10-28 17:52:57 -0300

    nvim: Fix double quote

--- dot_config/nvim/after/plugin/harpoon.lua
+++ dot_config/nvim/after/plugin/harpoon.lua
@@ -1,4 +1,4 @@
-require("telescope").load_extension('harpoon')
+require("telescope").load_extension("harpoon")
 
 vim.keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
 vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)

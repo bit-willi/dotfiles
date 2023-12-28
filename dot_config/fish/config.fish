f6a36068d6a38ee9766ac653e32bd893976ac0d6
Willian de Souza <willian.souza.ti@gmail.com>
2023-12-28 19:05:15 -0300

    fish: configure PATH

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
 
+    export PATH="$HOME/.bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$HOME/.emacs.d/bin:/home/$USER/.local/bin/"
+
     set -U fish_greeting ""
 
     source $HOME/.config/fish/aliases.fish

5bfa4a61d3768c7ee22b2a952d8b6afdae88a7f3
Willian de Souza <willian.souza.ti@gmail.com>
2024-01-02 19:18:40 -0300

    feat: add exports to fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -8,6 +8,7 @@ if status is-interactive
 
     source $HOME/.config/fish/aliases.fish
     source $HOME/.config/fish/git.fish
+    source $HOME/.config/fish/exports.fish
     source /etc/grc.fish
 
     set -g FZF_CTRL_T_COMMAND "command find -L \$dir -type f 2> /dev/null | sed '1d; s#^\./##'"

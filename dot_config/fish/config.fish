0b89deabccb1eede187064cd55fad9a7fa447c46
Willian de Souza <willian.souza.ti@gmail.com>
2024-04-05 19:49:55 -0300

    fish: add fzf key bindings

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -9,11 +9,10 @@ if status is-interactive
     source $HOME/.config/fish/aliases.fish
     source $HOME/.config/fish/git.fish
     source $HOME/.config/fish/exports.fish
+    source $HOME/.config/fish/fzf-key-bindings.fish
     source /etc/grc.fish
 
     set -g FZF_CTRL_T_COMMAND "command find -L \$dir -type f 2> /dev/null | sed '1d; s#^\./##'"
-
-    source $HOME/.local/share/nvim/lazy/fzf/shell/key-bindings.fish
 
     source $HOME/.asdf/asdf.fish
 

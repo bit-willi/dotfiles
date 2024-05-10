04b78715197dee256ad1c39d7efde58c3b2f6232
Willian de Souza <willian.souza.ti@gmail.com>
2024-05-10 17:11:39 -0300

    fish: handle pyenv

--- dot_config/fish/exports.fish
+++ dot_config/fish/exports.fish
@@ -2,3 +2,10 @@ export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
 #ASDF python binaries
 export PATH="$PATH:$HOME/.asdf/installs/python/3.12.1/bin"
 export EDITOR="$HOME/.bin/editor"
+export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+
+#Pyenv
+set -x PYENV_ROOT $HOME/.pyenv
+set -x fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source

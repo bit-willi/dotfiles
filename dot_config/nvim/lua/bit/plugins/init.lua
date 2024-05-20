f1b3f8622da5d0541000b9fb2fb3c4a27ff4cf60
Willian de Souza <willian.souza.ti@gmail.com>
2024-05-20 09:44:25 -0300

    nvim: load neorg export module

--- dot_config/nvim/lua/bit/plugins/init.lua
+++ dot_config/nvim/lua/bit/plugins/init.lua
@@ -124,7 +124,6 @@ return {
         dependencies = { "luarocks.nvim" },
         lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
         version = "*", -- Pin Neorg to the latest stable release
-        config = true,
     },
     {
         "luckasRanarison/nvim-devdocs",

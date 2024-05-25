0f8795065c10e72b8d76dda0da3bf068d6e8f230
Willian de Souza <willian.souza.ti@gmail.com>
2024-05-25 08:00:37 -0300

    nvim: handle neorg export to markdown

--- dot_config/nvim/after/plugin/neorg.lua
+++ dot_config/nvim/after/plugin/neorg.lua
@@ -1,14 +1,24 @@
-require("neorg").setup {
+require("neorg").setup({
     load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.dirman"] = { -- Manages Neorg workspaces
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.dirman"] = {
             config = {
                 workspaces = {
                     notes = "~/.journal",
                 },
+                default_workspace = "notes",
             },
         },
-        ["core.export"] = {}
+        ["core.export"] = {},
+        ["core.export.markdown"] = {
+            config = {
+                public = {
+                    export_dir = "~/.journal/posts",
+                },
+                extensions = "all",
+                export_dir = "~/Documents/codes/personal/blog/_posts"
+            }
+        }
     },
-}
+})

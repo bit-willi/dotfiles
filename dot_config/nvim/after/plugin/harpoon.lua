67487b287bf521d302a1a0747a57b1df6e35292d
Willian de Souza <willian.souza.ti@gmail.com>
2024-06-10 11:34:55 +0000

    nvim: remove old harpoon config

--- dot_config/nvim/after/plugin/harpoon.lua
+++ dot_config/nvim/after/plugin/harpoon.lua
@@ -1,10 +1,3 @@
-require("telescope").load_extension("harpoon")
-
-vim.keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
-vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
-vim.keymap.set("n", "<leader>hd", require("harpoon.mark").rm_file)
-
-
 local harpoon = require("harpoon")
 
 harpoon:setup()

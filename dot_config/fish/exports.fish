6566694bd276f011376c751623eb48109dddd128
Willian de Souza <willian.souza.ti@gmail.com>
2024-09-25 01:56:12 -0700

    fish: export DOCKER_HOST var

--- dot_config/fish/exports.fish
+++ dot_config/fish/exports.fish
@@ -4,6 +4,9 @@ export PATH="$PATH:$HOME/.asdf/installs/python/3.12.1/bin"
 export EDITOR="$HOME/.bin/editor"
 export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 
+# Docker rootless
+export DOCKER_HOST=unix:///run/user/1000/docker.sock
+
 #Pyenv
 set -x PYENV_ROOT $HOME/.pyenv
 set -x fish_user_paths $PYENV_ROOT/bin $fish_user_paths

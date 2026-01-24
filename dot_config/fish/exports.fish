586af49e724dd94367b1f141be64844b0ee706b4
Willian de Souza <me@bitwilli.com>
2026-01-24 10:45:48 -0300

    fish: update exports to handle android dev

--- dot_config/fish/exports.fish
+++ dot_config/fish/exports.fish
@@ -1,12 +1,12 @@
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 export EDITOR="$HOME/.bin/editor"
 export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
-export PATH="$PATH":"$HOME/.pub-cache/bin"
-
-# Docker rootless
+export ANDROID_HOME=/opt/android-sdk
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
-
 export CHROME_EXECUTABLE=/usr/bin/chromium
+
+# Path
+export PATH="$PATH":"$HOME/.pub-cache/bin:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/emulator"
 
 # Env Secrets
 source $HOME/.env-secrets
